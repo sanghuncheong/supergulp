19076774da0c59d34526f1f8feb1707388a54ba8
welovehun <welovehun91@gmail.com>
2020-12-07 17:00:06 +0900

    gulp is not working on the window...
    내가 못하는건지....
    우분투에서 try를 해볼것

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,10 @@ import del from "del";
 import ws from "gulp-webserver";
 import livereload from "connect-livereload";
 import image from "gulp-image";
+import sass from "gulp-sass";
 // const gulp = require("gulp");
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
@@ -17,6 +20,11 @@ const routes = {
     src: "src/img/*",
     dest: "build/img",
   },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
 };
 
 const pug = () =>
@@ -28,16 +36,23 @@ const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, port: 8001 }));
 
 const watch = () => {
-  gulp.watch(routes.pug.watch, pug);
-  gulp.watch(routes.img.src, img);
+  // gulp.watch(routes.pug.watch, pug);
+  // gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
 const img = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.series([webserver]);
 
